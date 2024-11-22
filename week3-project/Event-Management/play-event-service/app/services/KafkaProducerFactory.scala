7eb52e28fb76ea2e39e695be382f9bbf6153c722
Aman <aman@thoughtgenesis.com>
2024-11-22 11:44:47 +0530

    Added docker configs for event management

--- week3-project/Event-Management/play-event-service/app/services/KafkaProducerFactory.scala
+++ week3-project/Event-Management/play-event-service/app/services/KafkaProducerFactory.scala
@@ -20,7 +20,7 @@ object MessageTeam {
 class KafkaProducerFactory @Inject()() {
   private val props = new Properties()
 
-  props.put("bootstrap.servers", "localhost:9092")
+  props.put("bootstrap.servers", "kafka1:9092")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
